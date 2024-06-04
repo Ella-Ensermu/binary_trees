bb983ffb19b8f0ba47640ba7acf4d6f8d5fa85e1
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-04 13:42:59 +0300

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,23 +9,20 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *newer_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL)
-	{
+	newer_node = binary_tree_node(parent, value);
+	if (newer_node == NULL)
 		return (NULL);
-	}
+
 	if (parent->right != NULL)
 	{
-		new_node->right = parent->right;
-		parent->right->parent = new_node;
+		newer_node->right = parent->right;
+		parent->right->parent = newer_node;
 	}
-	parent->right = new_node;
-	return (new_node);
+	parent->right = newer_node;
+	return (newer_node);
 }
