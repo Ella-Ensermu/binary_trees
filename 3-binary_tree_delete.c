44af144da90250edf007b9b947ff52afffefc91b
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-04 13:58:29 +0300

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_delete - free an entire tree using recursion it free the node
- * when the node left and right are NULL
- * @tree: tree to free
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: tree to delete
  * Return: Nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
