8fff77bbd1731edbb90ef5684076592c9d8bb227
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-06 12:52:38 +0300

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,11 +4,13 @@
  * binary_tree_is_root - checks if a node is a root
  * @node: node to check
  *
- * Return: 1 if node is a root
- *         0 if not a root
- *         0 if node is NULL
+ * Return: 0 if is not a root 1 if it is
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	return ((!node || node->parent) ? 0 : 1);
+	if (node == NULL || node->parent != NULL)
+		return (0);
+{
+	return (1);
+}
 }
