35c5afe6e3e2debad12684d43f646028701def04
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-06 14:40:22 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,41 +1,37 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance factor
- *
- * Return: the balance factor
- *         0 if tree is NULL
- */
-int binary_tree_balance(const binary_tree_t *tree)
-{
-	int height_l, height_r;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
-	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
-
-	return (height_l - height_r);
-}
-
-/**
  * binary_tree_height - measures the height of a binary tree
  * @tree: tree to measure the height of
  *
- * Return: height of the tree
- *         0 if tree is NULL
+ * Return: If tree is NULL, your function must return 0, else return height.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
+	size_t height_left = 0;
+	size_t height_right = 0;
 
 	if (!tree)
 		return (0);
 
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	height_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_left > height_right ? height_left : height_right);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: If tree is NULL, return 0, else return balance factor.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	}
+	return (0);
 }
