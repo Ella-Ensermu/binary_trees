1adbff4d826753101d6a10321cc5b6bdf3c88d65
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-06 14:29:14 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,24 +6,17 @@
  * Return: the height
  */
 
-size_t binary_tree_height_b(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+	size_t heighter_left = 0;
+	size_t heighter_right = 0;
 
-	if (tree == NULL)
-	{
+	if (!tree)
 		return (0);
-	}
-	else
-	{
-		if (tree)
-		{
-			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
-		}
-		return ((l > r) ? l : r);
-	}
+
+	heighter_left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	heighter_right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (height_l > heighter_right ? heighter_left : heighter_right);
 }
 
 /**
