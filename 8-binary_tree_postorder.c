9ad990deb24eaee715444d04920f1bff5c100065
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-06 13:24:38 +0300

    Update 8-binary_tree_postorder.c

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder - print elements of tree using post-order traversal
+ * binary_tree_postorder - Traverses a binary tree using post-order traversal.
  * @tree: tree to go through
- * @func: function to use
+ * @func:  pointer to a function to call for each node.
  * Return: Nothing
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || func == NULL)
+	if (!tree || !func)
 	{
 		return;
 	}
