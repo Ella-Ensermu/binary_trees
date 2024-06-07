0a1d53c08bdcc677d374b4033a4cd12bb302264b
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-07 11:34:39 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,7 +3,9 @@
 /**
  * binary_tree_sibling - function that finds if a node is sibling
  * @node: node to check
- * Return: The sibling node
+ *
+ * Return: If node is NULL or there is no sibling - NULL.
+ *         Otherwise - a pointer to the sibling.
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
