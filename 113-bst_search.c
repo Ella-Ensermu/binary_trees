97eaedbb4d7504c35a3c770dc9df28625e48aabc
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-07 12:27:11 +0300

    Update 113-bst_search.c

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -3,20 +3,20 @@
 /**
  * bst_search - Searches for a value in a binary search tree.
  * @tree: A pointer to the root node of the BST to search.
- * @value: The value to search for in the BST.
+ * @name: The value to search for in the BST.
  *
  * Return: If the tree is NULL or the value is not found, NULL.
  *         Otherwise, a pointer to the node containing the value.
  */
-bst_t *bst_search(const bst_t *tree, int value)
+bst_t *bst_search(const bst_t *tree, int name)
 {
 	if (tree != NULL)
 	{
-		if (tree->n == value)
+		if (tree->n == name)
 			return ((bst_t *)tree);
-		if (tree->n > value)
-			return (bst_search(tree->left, value));
-		return (bst_search(tree->right, value));
+		if (tree->n > name)
+			return (bst_search(tree->left, name));
+		return (bst_search(tree->right, name));
 	}
 	return (NULL);
 }
