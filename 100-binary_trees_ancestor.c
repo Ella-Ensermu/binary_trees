4f747293bed71bc26c67f5f99e12fba0e0d9c092
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-06-07 11:49:47 +0300

    Update 100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,25 +2,26 @@
 
 /**
  * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
- * @first: Pointer to the first node.
- * @second: Pointer to the second node.
- *
- * Return: If no common ancestors return NULL, else return common ancestor.
+  @first: First node
+ * @second: Second node
+ * Return: the node of the ancestor
  */
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second)
 {
-	binary_tree_t *mom, *pop;
+	binary_tree_t *a, *b;
 
 	if (!first || !second)
+	{
 		return (NULL);
+	}
 	if (first == second)
 		return ((binary_tree_t *)first);
-
-	mom = first->parent, pop = second->parent;
-	if (first == pop || !mom || (!mom->parent && pop))
-		return (binary_trees_ancestor(first, pop));
-	else if (mom == second || !pop || (!pop->parent && mom))
-		return (binary_trees_ancestor(mom, second));
-	return (binary_trees_ancestor(mom, pop));
+	a = first->parent, b = second->parent;
+	if (first == b || !a || (!a->parent && b))
+		return (binary_trees_ancestor(first, b));
+	else if (a == second || !b || (!b->parent && a))
+		return (binary_trees_ancestor(a, second));
+	return (binary_trees_ancestor(a, b));
 }
